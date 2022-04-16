72ca09b2f99444fdaccc80eb71ba3fbb6f77daff
pvesouza <pvesouza@gmail.com>
2022-04-16 00:24:19 -0300

    binarization based on histogram image

--- main.cpp
+++ main.cpp
@@ -13,14 +13,19 @@ int top_slider = 10;
 int top_slider_max = 200;
 char TrackbarName[50];
 
+// Saves the rectangle points in a txt file
 void savePoints(Point *points, int number, char *path) {
     FileHelper helper;
         // Saves the name of the arquive
     char c = '\0', name[100];
+    int secondPoint = 0;
     uint8_t count = 0;
 
-    while ((c != '.') && (count < 100)) {
+    while ((secondPoint < 2) && (count < 100)) {
         c = path[count];
+        if (c == '.') {
+            secondPoint++;
+        }
         name[count++] = c;
     }
 
@@ -28,17 +33,18 @@ void savePoints(Point *points, int number, char *path) {
     name[count++] = 't';
     name[count++] = 'x';
     name[count++] = 't';
-    
+
     helper.saveFile(points, number, name);
 }
 
+// Trackbar for Canny border algorithm
 void on_trackbar_canny(int, void*) {
     Canny(image, border, top_slider, 3 * top_slider);
     imshow("canny", border);
 }
 
 // Histogram Equalization Function
-Mat plotHistogram(cv::Mat image) {
+Mat histogramEqualize(cv::Mat image) {
     // Equalize histogram
     Mat hist_equalized_image;
     equalizeHist(image, hist_equalized_image);
@@ -50,62 +56,110 @@ Mat plotHistogram(cv::Mat image) {
     return hist_equalized_image;
 }
 
+Mat plotHistogram(const Mat &image) {
+    int histSize = 256;
+    float range[] = {0,256};
+    const float *histRange[] = {range};
+
+    bool uniform = true, accumulate = false;
+
+    Mat grayHistogram;
+    calcHist(&image, 1, 0, Mat(), grayHistogram, 1, &histSize, histRange, uniform, accumulate);
+
+    int histWidth = 512, histHeight = 400;
+    int binWidth = cvRound((double) histWidth / histSize);
+
+    Mat histImage(histHeight, histWidth, CV_8UC3, Scalar(0,0,0));
+
+    normalize(grayHistogram, grayHistogram, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+
+    for (int i = 1; i < histSize; i++) {
+
+        line(histImage, Point(binWidth*(i - 1), histHeight - cvRound(grayHistogram.at<float>(i - 1)))
+                ,Point(binWidth*(i), histHeight - cvRound(grayHistogram.at<float>(i)) )
+                ,Scalar(255, 0, 0), 2, 8, 0);
+    }
+
+    imshow("histogram calculated", histImage);
+
+    return grayHistogram;
+}
+
+Mat dilateImage(const Mat &image) {
+
+    int dilation_type = MORPH_RECT;
+    int dilation_size = 3;
+    Mat dilation_dst;
+
+    Mat element = getStructuringElement( 
+            dilation_type,
+            Size( 2*dilation_size + 1, 2*dilation_size+1 ),
+            Point( dilation_size, dilation_size ) );
+
+  dilate( image, dilation_dst, element );
+
+  imshow( "Dilation Demo", dilation_dst );
+  return dilation_dst;
+}
+
+//***********************************************************************
+/////////MAIN
+//***********************************************************************
+
 int main(int argc, char** argv) {
 
     Point rectangleCorners[4] = {{10,0}, {20,0}, {10,200}, {20,200}};
-
     // Reads an image from arquive
     image = cv::imread(argv[1],cv::IMREAD_GRAYSCALE);
 
     if (image.empty()) {
-        return 1;
+        return EXIT_FAILURE;
     }
 
     savePoints(rectangleCorners, 4, argv[1]);
+    imshow("image", image);
 
-    cv::imshow("image", image);
+    Mat hist = plotHistogram(image);
 
-    sprintf( TrackbarName, "Threshold inferior", top_slider_max);
 
-    namedWindow("canny",1);
+    float great = 0.0;
+    int index = 0;
 
-    createTrackbar( TrackbarName, 
-                "canny",
-                &top_slider,
-                top_slider_max,
-                on_trackbar_canny);
-
-    on_trackbar_canny(top_slider, &top_slider);
-
-    imwrite("cannyborders.png", border);
-
-    imshow("Canny", border);
-
-    //Storing the histogram
-    Mat histogram;
-    histogram = Mat::zeros(1, 256, CV_8U);
-
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            uchar pixelValue = image.at<uchar>(i,j);
-            // Incrementa a quantidade daquela posição
-            histogram.at<uchar>(1, pixelValue) += 1;
+    for (int j = 0; j < hist.rows; j++) {
+        cout << "(" << j << "," << 0 << ") = " << hist.at<float>(j,0) << endl;
+        if (great < hist.at<float>(j,0)) {
+            great = hist.at<float>(j,0);
+            index = j;
         }
     }
 
-    //Calculando a média ponderada dos pixels pelo histograma
-    int sum = 0;
-    int index = 0;
-    for (int i = 0; i < histogram.cols; i++) {
-        if (sum < histogram.at<uchar>(0,i)) {
-            sum = histogram.at<uchar>(0,i);
-            index = i;
-        }
+    cout << "index: " << index << endl;
+    cout << "maior: " << great << endl;
+
+    //sprintf( TrackbarName, "Threshold inferior", top_slider_max);
+
+    //namedWindow("canny",1);
+
+    //createTrackbar( TrackbarName, 
+      //          "canny",
+        //        &top_slider,
+          //      top_slider_max,
+            //    on_trackbar_canny);
+
+    //on_trackbar_canny(top_slider, &top_slider);
+
+    //imshow("Canny", border);
+
+    if (index == 255) {
+        index -= 100;
+    }else {
+        int dist = (255 - index) / 3;
+        index += dist;
     }
 
     // Making Image binary
     for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < border.cols ; j++) {
+        for (int j = 0; j < image.cols ; j++) {
             uchar value = 0;
             value = image.at<uchar>(i,j);
 
@@ -118,6 +172,11 @@ int main(int argc, char** argv) {
     }
 
     imshow("Binarization", image);
+
+    image = dilateImage(image);
+
+    //border = dilateImage(border);
+
     waitKey();
 
     return 0;
