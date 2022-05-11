31fdcd412362e385a76521d3fff3e9d0a77f49f8
pvesouza <pvesouza@gmail.com>
2022-05-11 13:53:37 -0300

    windows visual studio

--- fileHelper.cpp
+++ fileHelper.cpp
@@ -9,7 +9,28 @@ FileHelper::~FileHelper() {
 }
 
 void FileHelper::saveFile(Point *points, int nPoints, const char *path) {
-    ofstream outputFile(path);                                              // Creates an output file
+    //ofstream outputFile;                                           // Creates an output file
+    //cout << "Passou Aqui" << endl;
+    //outputFile.open(path, ios::out);
+    //cout << "Failure to open file" << endl;
+    //if (!outputFile.is_open()) {
+        //cout << "Failure to open file" << endl;
+        //return;
+    //}
+    bool isEmpty = true;
+    for (int i = 0; i < nPoints; i++)
+    {
+        if (points[i].x != 0 || points[i].y != 0) {
+            isEmpty = false;
+        }
+    }
+
+    if (isEmpty)
+    {
+        cout << "[]" << endl;
+        return;
+    }
+
     char jsonOut[201];
     char jsonAux[50];
     int pos = 0;
@@ -17,24 +38,28 @@ void FileHelper::saveFile(Point *points, int nPoints, const char *path) {
     jsonOut[0] = '\0';
 
     // Writes the points in file
-    for (int i = 0; i < nPoints; i++) {
+    for (int i = 0; i < nPoints; i++) 
+    {
         Point p = *(points + i);
         int writtenChars = 0;
 
         if (i == 0) {
-            writtenChars = sprintf(jsonAux, "{\n\"c%d\":\"%d\",\"%d\",\n", i + 1, p.x, p.y);
+            writtenChars = sprintf(jsonAux, "{\"c%d\":\"%d,%d\",", i + 1, p.x, p.y);
         } else if (i == 3) {
-            writtenChars = sprintf(jsonAux, "\"c%d\":\"%d\",\"%d\",\n}", i + 1, p.x, p.y);
+            writtenChars = sprintf(jsonAux, "\"c%d\":\"%d,%d\"}", i + 1, p.x, p.y);
         }else {           
-            writtenChars = sprintf(jsonAux, "\"c%d\":\"%d\",\"%d\",\n", i + 1, p.x, p.y);
+            writtenChars = sprintf(jsonAux, "\"c%d\":\"%d,%d\",", i + 1, p.x, p.y);
         }
+
         pos = pos + writtenChars;
         if (pos > 201){
-            break;
+            cout << "Name file very large" << endl;
+            return;
         }
-        strcat(jsonOut, jsonAux);
+        cout << jsonAux << endl;
+        strcat_s(jsonOut, jsonAux);
     }
-    outputFile << jsonOut << endl;
+    //outputFile << jsonOut << endl;
     // Closes files 
-    outputFile.close();
+    //outputFile.close();
 }
